8ba6a29fcf8a64a66f2827a0da89f1fa215eaca6
thanapolkhunkoei <78915927+thanapolkhunkoei@users.noreply.github.com>
2021-02-20 21:24:46 +0700

    Excercise4

---
+++ Excercise4_Thanapol_k.py
@@ -0,0 +1,15 @@
+o = "--- Your Score ---"
+a = "Foundation English"
+b = "General Business"
+c = "Introduction to Computer Systems"
+d = "Computer Programming"
+e = ":"
+f = 97.8
+g = 98
+h = 97.7
+i = 94
+print(o)
+print(a, f)
+print(b, g)
+print(c, h)
+print(d, i)
