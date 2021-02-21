9d20356595c72352e5a5c486d1cdadbed8fdb993
thanapolkhunkoei <78915927+thanapolkhunkoei@users.noreply.github.com>
2021-02-21 20:17:37 +0700

    Excercise5_1_Thanapol_k

---
+++ Excercise5_1_Thanapol_k.py
@@ -0,0 +1,12 @@
+num1 = int(input("num1 : "))
+operator = input("operator : ")
+num2 = int(input("num2 : "))
+
+if operator == "+" :
+    print(num1 + num2)
+elif operator == "-" :
+    print(num1 - num2)
+elif operator == "*" :
+    print(num1 * num2)
+elif operator == "/" :
+    print(num1 / num2)
